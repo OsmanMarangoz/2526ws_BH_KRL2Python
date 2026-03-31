51e2d1abb38a49a96b230673f0b106b85dd61621
OsmanMarangoz <marangozit@gmail.com>
2026-03-31 12:04:24 +0200

    circ,pathplanning scirpt example, und databases mit den pfaden

---
+++ scripts/path_planing_example.py
@@ -0,0 +1,54 @@
+from time import sleep
+from pathlib import Path
+import sys
+
+project_root = Path(__file__).resolve().parents[1]
+src_dir = project_root / "src"
+if str(src_dir) not in sys.path:
+    sys.path.insert(0, str(src_dir))
+
+from robot import Robot
+from command import Command
+from point import Point6D
+from csvHelper import load_all_points_csv, load_point_csv
+
+if __name__ == "__main__":
+    database_dir = project_root / "database"
+
+    KUKA_IP = "10.181.116.51"
+    KUKA_PORT_META = 54601
+    KUKA_PORT_MOTION = 54602
+
+    kuka = Robot(KUKA_IP, KUKA_PORT_META, KUKA_PORT_MOTION)
+    command = Command(kuka)
+
+    kuka.connect()
+
+    if kuka.motion_transport.connected:
+        kuka.start_receive_threads()
+        command.start_safety_thread()
+
+        # Default settings
+        kuka.set_default_velocity(0.1)
+        kuka.set_default_base(15)
+        kuka.set_default_tool(15)
+
+        sequence_points_prm = load_all_points_csv(str(database_dir / "prm.csv"))
+        sequence_points_astar = load_all_points_csv(str(database_dir / "Astar.csv"))
+
+
+        kuka.move_sequence(
+            points=sequence_points_prm,
+            mode=3,
+            vel=0.10
+        )
+        sleep(1)
+
+        kuka.move_sequence(
+            points=sequence_points_astar,
+            mode=3,
+            vel=0.10
+        )
+
+
+        kuka.disconnect()
